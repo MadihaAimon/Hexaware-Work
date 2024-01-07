1d1fcf606c03c0d62ff3207da5778df4dae69453
MadihaAimon <153699931+MadihaAimon@users.noreply.github.com>
2024-01-07 10:24:14 +0530

    Update db_connection.py

--- Case Study/util/db_connection.py
+++ Case Study/util/db_connection.py
@@ -3,23 +3,15 @@ import mysql.connector
 
 def get_connection():
     # Read the properties from the file
-    with open(r'C:\Users\madih\PycharmProjectsVirtualartgallery\pythonProject\util\config.properties', 'r') as file:
-        lines = file.readlines()
-        properties = {}
-        for line in lines:
-            if '=' in line:
-             key, value = line.strip().split('=',1)
-             properties[key.strip()] = value.strip()
-
-    # Create the connection string
-    connection_string = f"mysql://{properties['username']}:{properties['password']}@{properties['hostname']}:{properties['port']}/{properties['dbname']}"
-
-    try:
-        connection = mysql.connector.connect(host=properties['hostname'],
-                                             database=properties['dbname'],
-                                             user=properties['username'],
-                                             password=properties['password'])
+       try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="Root",
+            port='3306',
+            database=" virtualartgallery"
+        )
         return connection
-    except mysql.connector.Error as e:
+    except Error as e:
         print(f"Error connecting to the database: {e}")
         return None
